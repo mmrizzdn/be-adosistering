c621e3a81d317f30f0c00cf0e60637d6abd21782
mmrizzdn <zoed.eldien15@gmail.com>
2024-03-06 00:27:03 +0700

    app

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(usersRouter);
+app.use('/login', usersRouter);
 app.use('/firebase', firebaseRoutes);
 
 app.listen(port, () => {
