dc863965bab0f2e3d20a7a2b1332ac1de0fb4ee4
mmrizzdn <zoed.eldien15@gmail.com>
2024-03-06 00:10:25 +0700

    firebase

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(usersRouter);
-app.use('/', firebaseRoutes);
+app.use('/firebase', firebaseRoutes);
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);
